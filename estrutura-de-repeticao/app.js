03b1d5fcdf0ab31c6897929f90ec76d38deb7d48
pacicco <102540905+pacicco@users.noreply.github.com>
2022-04-20 16:52:59 -0300

    lista estrutura repetição

--- estrutura-de-repeticao/app.js
+++ estrutura-de-repeticao/app.js
@@ -1,7 +1,27 @@
-const listaArgumentos = process.argv.slice(2); 
+const listaArgumentos = process.argv.slice(2);
 
-console.log ('---------------------Executando um FOR-------------------------');
-for (let controladorFor = 0; controladorFor < listaArgumentos.length; controladorFor++){
-    console.log ('Posicao ${controladoFor} valor lido = ${listaArgumentos[controladorFor]}');
+console.log('---------------------Executando um FOR-------------------------');
+for (let controladorFor = 0; controladorFor < listaArgumentos.length; controladorFor++) {
+    console.log(`Posicao ${controladorFor} valor lido = ${listaArgumentos [controladorFor]}`);
 }
 
+console.log('---------------------Executando um WHILE-------------------------');
+let controladorWhile = 0;
+while (controladorWhile < listaArgumentos.length){
+    console.log (`Posicao ${controladorWhile} valor lido = ${listaArgumentos[controladorWhile]}`);
+    controladorWhile++;
+}
+
+console.log('---------------------Executando um DO WHILE-------------------------');
+let controladorDoWhile = 0;
+do {
+    console.log (`Posicao ${controladorDoWhile} valor lido = ${listaArgumentos[controladorDoWhile]}`);
+    controladorDoWhile++;
+} while (controladorDoWhile < listaArgumentos.length)
+
+console.log ('-----------------------Executando um FOR OF----------------');
+
+
+for (const argumento of listaArgumentos){
+    console.log (`Valor lido = ${argumento}`);
+} 
