7309208a742b405ca6893a50783dfde29987e76d
pacicco <102540905+pacicco@users.noreply.github.com>
2022-04-27 17:46:23 -0300

    calculadora concluida

--- operadores-e-functions/app.js
+++ operadores-e-functions/app.js
@@ -1,19 +1,17 @@
 const readLine = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input : process.stdin,
+    output : process.stdout
 });
 
-const validarNumeroInformado = (numero) => {
-    const resultado = Number.parseFloat(numero);
-    if (!resultado) {
-        console.log(`numero informado nao e valido`);
-    }
-    return resultado
-
+const validarNumeroInformado = numero => {
+        const resultado = Number.parseFloat(numero);
+        if(!resultado){
+            console.log(`numero informado nao e valido`);
+        }
+        return resultado
 }
-
 const validarOperador = (operador) => {
-    switch (operador) {
+    switch(operador){
         case '+':
         case '-':
         case '/':
@@ -22,39 +20,36 @@ const validarOperador = (operador) => {
             return operador;
         default:
             console.log('operador informado e invalido');
-            return null;
-
+            return null;        
     }
 }
-readLine.question('Favor informar um numero:', (numero1) => {
+
+readLine.question('Favor informar um numero:', numero1 => {
     const numeroValidado1 = validarNumeroInformado(numero1);
 
-    if (numeroValidado1) {
-        readLine.question('Favor Informar outro numero:'), (numero2) => {
+    if(numeroValidado1){
+        readLine.question('Favor informar outro numero:', (numero2) => {
             const numeroValidado2 = validarNumeroInformado(numero2);
-
-            if (numeroValidado2) {
+            if(numeroValidado2){
                 readLine.question('Favor informar o operador:', (operador) => {
                     const operadorValidado = validarOperador(operador);
-                    if (operadorValidado) {
-                        switch (operadorValidado) {
-                            case '+': console.log(`operador selecionado adicao resultado ${numeroValidado1} + ${numeroValidado2} = ${numeroValidado1} + ${numeroValidado2}`);
+                    if(operadorValidado){
+                        switch(operadorValidado){
+                            case '+' : console.log(`operador selecionado adicao resultado ${numeroValidado1} + ${numeroValidado2} = ${numeroValidado1 + numeroValidado2}`);
                                 break;
-                            case '-': console.log(`operador selecionado subtracao resultado ${numeroValidado1} - ${numeroValidado2} = ${numeroValidado1} - ${numeroValidado2}`);
+                            case '-' : console.log(`operador selecionado subtracao resultado ${numeroValidado1} - ${numeroValidado2} = ${numeroValidado1 - numeroValidado2}`);
                                 break;
-                            case '*': console.log(`operador selecionado multiplicacao resultado ${numeroValidado1} * ${numeroValidado2} = ${numeroValidado1} * ${numeroValidado2}`);
+                            case '*' : console.log(`operador selecionado multiplicacao resultado ${numeroValidado1} * ${numeroValidado2} = ${numeroValidado1 * numeroValidado2}`);
                                 break;
-                            case '/': console.log(`operador selecionado divisao resultado ${numeroValidado1} / ${numeroValidado2} = ${numeroValidado1} / ${numeroValidado2}`);
+                            case '/' : console.log(`operador selecionado divisao resultado ${numeroValidado1} / ${numeroValidado2} = ${numeroValidado1 / numeroValidado2}`);
                                 break;
-                            case '%': console.log(`operador selecionado modulo resultado ${numeroValidado1} % ${numeroValidado2} = ${numeroValidado1} % ${numeroValidado2}`);
+                            case '%' : console.log(`operador selecionado modulo resultado ${numeroValidado1} % ${numeroValidado2} = ${numeroValidado1 % numeroValidado2}`);
                                 break;
                             default: break;
-
                         }
                     }
                 });
             }
-        }
+        });
     }
-
 })
